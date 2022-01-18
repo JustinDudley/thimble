b59ea918a86abc790d207383e0230587201294e1
Justin Dudley <justindudley99@gmail.com>
2022-01-18 17:04:52 -0500

    rename files

--- src/thimbleRoutes/index.tsx
+++ src/thimbleRoutes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './Home/home';
-import { Keyboard } from './Keyboard/keyboard';
+import { Home } from './Home';
+import { Keyboard } from './Keyboard';
 
 export const ThimbleRoutes: React.FC = () => {
 
