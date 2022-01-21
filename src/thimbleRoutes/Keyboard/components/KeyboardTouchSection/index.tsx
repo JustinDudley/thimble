5f1baf3a6e2cca124ee3f7e47eaf5c0c49b41633
Justin Dudley <justindudley99@gmail.com>
2022-01-21 14:31:52 -0500

    use map to create the 4 letterBoxes

--- src/thimbleRoutes/Keyboard/components/KeyboardTouchSection/index.tsx
+++ src/thimbleRoutes/Keyboard/components/KeyboardTouchSection/index.tsx
@@ -4,15 +4,14 @@ import { LetterBox } from '../LetterBox'
 import './style.css'
 
 export const KeyboardTouchSection: React.FC = () => {
+    const array_4 = new Array(4).fill('');
+
     return (
         <>
             <div id='keyboardTouchSection'>
                 hello from keyboardTouchSection
             <div id='keyboardRow'>
-                <LetterBox />
-                <LetterBox />
-                <LetterBox />
-                <LetterBox />
+                {array_4.map(() => <LetterBox />)}
             </div>
             </div>
         </>
