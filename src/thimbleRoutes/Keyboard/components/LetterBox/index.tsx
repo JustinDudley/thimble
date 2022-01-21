9059248666c145757835d16f21f598c58224f419
Justin Dudley <justindudley99@gmail.com>
2022-01-21 14:28:05 -0500

    use map to create the 15 mini boxes

--- src/thimbleRoutes/Keyboard/components/LetterBox/index.tsx
+++ src/thimbleRoutes/Keyboard/components/LetterBox/index.tsx
@@ -16,28 +16,15 @@ const Mini: React.FC = () => {
 }
 
 export const LetterBox: React.FC = () => {
+    const array_15 = new Array(15).fill('')
+    
     return (
         <div id="letterBox">
             <div id="letterShadow" />
-
             <div id='theLetter'>k</div> 
             {/* <img style={{position: 'absolute', top: '20%', left: '20%', width: '60%', height: '60%' , zIndex: '-2'}} src={require("../../../../assets/images/letter_D.png")} alt='letter' width='100%' height='100%' /> */}
             
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini />
-            <Mini /> 
+            {array_15.map(() => <Mini />)}
         </div>
     )
 }
