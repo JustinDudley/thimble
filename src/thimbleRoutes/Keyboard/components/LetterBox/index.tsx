8d5c1ff3a34a356641ceb2a78238b4cb72481a8b
Justin Dudley <justindudley99@gmail.com>
2022-01-21 14:05:06 -0500

    eliminate mini-row intermediate level. Add shaded region and letter k, and D png file but commented-out

--- src/thimbleRoutes/Keyboard/components/LetterBox/index.tsx
+++ src/thimbleRoutes/Keyboard/components/LetterBox/index.tsx
@@ -11,30 +11,54 @@ const Mini: React.FC = () => {
             id="mini" 
             onClick={() => {console.log('mini clicked'); setIsClicked(true);}}
             style={{backgroundColor: isClicked? 'red': 'inherit'}}
-        >
-            
-        </div>
-    )
-}
-
-const MiniRow: React.FC = () => {
-    return (
-        <div id='miniRow'>
-            <Mini />
-            <Mini />
-            <Mini />
-        </div>
+        />
     )
 }
 
 export const LetterBox: React.FC = () => {
     return (
         <div id="letterBox">
-            <MiniRow />
-            <MiniRow />
-            <MiniRow />
-            <MiniRow />
-            <MiniRow />
+            <div 
+                style={{
+                    position: 'absolute', 
+                    top: '10%', 
+                    left: '10%', 
+                    width: '80%', 
+                    height: '80%', 
+                    backgroundColor: 'lightgray', 
+                    zIndex: '-1'
+                }}
+            />
+
+             <div 
+                style={{
+                    position: 'absolute', 
+                    top: '5%', 
+                    left: '40%', 
+                    zIndex: '-1',
+                    fontSize: '2.5rem',
+                }}
+            >
+                k
+            </div> 
+            {/* <img style={{position: 'absolute', top: '20%', left: '20%', width: '60%', height: '60%' , zIndex: '-2'}} src={require("../../../../assets/images/letter_D.png")} alt='letter' width='100%' height='100%' /> */}
+            
+  
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini />
+            <Mini /> 
         </div>
     )
 }
