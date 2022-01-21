5a8755e031c1e9751213a9e662aec6b901857bd4
Justin Dudley <justindudley99@gmail.com>
2022-01-21 15:08:12 -0500

    only latest 3 touches show red in any given letter

--- src/thimbleRoutes/Keyboard/components/LetterBox/index.tsx
+++ src/thimbleRoutes/Keyboard/components/LetterBox/index.tsx
@@ -2,29 +2,40 @@ import React, { useState } from 'react';
 
 import './style.css'
 
-const Mini: React.FC = () => {
-
+const Mini: React.FC<{count: number}> = ({count}) => {
+    const numShown = 3;
     const [isClicked, setIsClicked] = useState(false);
+    const [miniCount, setMiniCount] = useState(0);
 
     return (
         <div 
             id="mini" 
-            onClick={() => {console.log('mini clicked'); setIsClicked(true);}}
-            style={{backgroundColor: isClicked? 'red': 'inherit'}}
-        />
+            onClick={() => {
+                setIsClicked(true);
+                setMiniCount(count); 
+            }}
+            style={{backgroundColor: (isClicked && count - (miniCount + 1) < numShown)? 'red': 'inherit'}}
+        >
+            {count},
+            {miniCount}
+        </div>
     )
 }
 
 export const LetterBox: React.FC = () => {
     const array_15 = new Array(15).fill('')
-    
+    const [count, setCount] = useState(0)
+
     return (
-        <div id="letterBox">
+        <div 
+            id="letterBox"
+            onClick={() => {setCount(count + 1);console.log(count)}}
+        >
             <div id="letterShadow" />
             <div id='theLetter'>k</div> 
             {/* <img style={{position: 'absolute', top: '20%', left: '20%', width: '60%', height: '60%' , zIndex: '-2'}} src={require("../../../../assets/images/letter_D.png")} alt='letter' width='100%' height='100%' /> */}
             
-            {array_15.map(() => <Mini />)}
+            {array_15.map(() => <Mini count={count} />)}
         </div>
     )
 }
