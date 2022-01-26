7186b5ede093eb3942c1f6cc5a122a776f50cd5a
Justin Dudley <justindudley99@gmail.com>
2022-01-26 13:59:42 -0500

    change variable names

--- src/thimbleRoutes/Keyboard/components/WholeKey/index.tsx
+++ src/thimbleRoutes/Keyboard/components/WholeKey/index.tsx
@@ -3,25 +3,25 @@ import { colors } from '../../../../helpers/jsColors';
 
 import './style.css'
 
-const Mini: React.FC<{count: number}> = ({count}) => {
-    const numShown = 4;
+const MiniBox: React.FC<{count: number}> = ({count}) => {
+    const numMiniShown = 4;
     const [isClicked, setIsClicked] = useState(false);
     const [miniCount, setMiniCount] = useState(0);
 
     return (
         <div 
-            id="mini" 
+            id="mini-box" 
             onClick={() => {
                 setIsClicked(true);
                 setMiniCount(count); 
             }}
-            style={{backgroundColor: isClicked && count === miniCount + 1? colors.purpleFeedback : isClicked && count - (miniCount + 1) < numShown? colors.purpleFaded: 'inherit'}}
+            style={{backgroundColor: isClicked && count === miniCount + 1? colors.purpleFeedback : isClicked && count - (miniCount + 1) < numMiniShown? colors.purpleFaded: 'inherit'}}
         />
     )
 }
 
 export const WholeKey: React.FC<{letter: string}> = ({letter}) => {
-    const array_15 = new Array(15).fill('')
+    const miniBoxes = new Array(15).fill('')
     const [count, setCount] = useState(0)
 
     return (
@@ -30,7 +30,7 @@ export const WholeKey: React.FC<{letter: string}> = ({letter}) => {
             style={{ width: letter === ' '? '47vw': '9.5vw'}}
             onClick={() => {setCount(count + 1);console.log(count)}}
         >
-            {array_15.map(() => <Mini count={count} />)}
+            {miniBoxes.map(() => <MiniBox count={count} />)}
             <div id='shown-key'>{letter}</div> 
             {/* <img style={{pointerEvents: 'none', position: 'absolute', top: '5%', left: '5%', width: '90%', height: '90%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '6px', boxSizing: 'border-box', border: '1px solid black'}} src={require("../../../../assets/images/letter_D.png")} alt='letter' width='100%' height='100%' /> */}
         </div>
