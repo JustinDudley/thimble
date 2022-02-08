7ac07909e52f53f6d0f8b3291fa310f8c098b48c
Justin Dudley <justindudley99@gmail.com>
2022-02-08 16:05:59 -0500

    create layout spots for user console items:  sentence, score, reset button

--- src/routes/TypingPage/components/SampleSection/index.tsx
+++ src/routes/TypingPage/components/SampleSection/index.tsx
@@ -3,5 +3,11 @@ import React from 'react';
 import styles from './style.module.css';
 
 export const SampleSection: React.FC = () => {
-   return <div className={styles.sampleSection}>sample text here</div>;
+   return (
+      <div className={styles.sampleSection}>
+         Not only is it the first time since 1993 that both conference No. 1
+         seeds have advanced to the Super Bowl, but this year also dispels the
+         notion that you need a dominant defense to win a championship.
+      </div>
+   );
 };
--- src/routes/TypingPage/components/UserConsole/index.tsx
+++ src/routes/TypingPage/components/UserConsole/index.tsx
@@ -2,6 +2,36 @@ import React from 'react';
 
 import styles from './style.module.css';
 
+const UserSentence: React.FC = () => {
+   return (
+      <div className={styles.userSentence}>
+         dynamically changing sentence goes here
+      </div>
+   );
+};
+
+const Panel: React.FC = () => {
+   return (
+      <div className={styles.panel}>
+         <Score />
+         <ResetButton />
+      </div>
+   );
+};
+
+const Score: React.FC = () => {
+   return <div>score</div>;
+};
+
+const ResetButton: React.FC = () => {
+   return <div>reset</div>;
+};
+
 export const UserConsole: React.FC = () => {
-   return <div className={styles.userConsole}>user section</div>;
+   return (
+      <div className={styles.userConsole}>
+         <UserSentence />
+         <Panel />
+      </div>
+   );
 };
