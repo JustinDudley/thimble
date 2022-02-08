0f6f181063dfcdacb48eb0820c0b919c8e078c40
Justin Dudley <justindudley99@gmail.com>
2022-02-08 14:18:45 -0500

    change lots of filenames and component names

--- src/routes/TypingPage/index.tsx
+++ src/routes/TypingPage/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { SampleRegion } from './components/SampleRegion';
 import { HomeLink } from './components/HomeLink';
 import { KeyboardRegion } from './components/KeyboardRegion';
+import { UserRegion } from './components/UserRegion';
 
 import styles from './style.module.css';
 
-export const Keyboard: React.FC = () => {
+export const TypingPage: React.FC = () => {
    return (
-      <div className={styles.keyboardPageViewHeight}>
-         <div className={styles.keyboardPage}>
+      <div className={styles.typingPageViewHeight}>
+         <div className={styles.typingPage}>
             <HomeLink />
             <SampleRegion />
+            <UserRegion />
             <KeyboardRegion />
          </div>
       </div>
