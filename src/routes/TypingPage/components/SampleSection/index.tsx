6d0f4031acad4629284e881480f839fcd772a473
Justin Dudley <justindudley99@gmail.com>
2022-02-09 13:23:01 -0500

    change words in sample area

--- src/routes/TypingPage/components/SampleSection/index.tsx
+++ src/routes/TypingPage/components/SampleSection/index.tsx
@@ -5,9 +5,15 @@ import styles from './style.module.css';
 export const SampleSection: React.FC = () => {
    return (
       <div className={styles.sampleSection}>
-         Not only is it the first time since 1993 that both conference No. 1
-         seeds have advanced to the Super Bowl, but this year also dispels the
-         notion that you need a dominant defense to win a championship.
+         <div style={{ fontWeight: 'bold', marginBottom: '20px' }}>
+            <div>For those about to type: We salute you!</div>
+            <div>Here is a sample sentence you can type:</div>
+         </div>
+         <div>
+            Not only is it the first time since 1993 that both conference No. 1
+            seeds have advanced to the Super Bowl, but this year also dispels
+            the notion that you need a dominant defense to win a championship.
+         </div>
       </div>
    );
 };
